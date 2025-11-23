88a861e117c751ed0523a3940afc7104a3e8b32c
BMBenkyou <178959326+BMBenkyou@users.noreply.github.com>
2025-11-24 05:46:40 +0800

    Create reusable components - (lift the state up)

--- practice/src/App.jsx
+++ practice/src/App.jsx
@@ -1,7 +1,28 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+    return (
+      <div>{props.counter}</div>
+    )
+}
+const Button = (props) => {
+    return (
+      <button onClick = {props.onClick}>
+        {props.text}
+      </button>
+    )
+
+}
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const descreaseByOne = () => setCounter(counter - 1)
+  const setToZero= () => setCounter(0)
+
+
+
+
 
   // const handleClick = () => {
   //   console.log ('Button clicked')
@@ -14,8 +35,10 @@ const App = () => {
 
   return (
     <div>
-    <div>{counter}</div>
-    <button onClick = {() => setCounter(counter+1)}> Click to increment</button>
+    <Display counter = {counter}/>
+    <button onClick = {increaseByOne} text = "increase by one "> + 1</button>
+    <button onClick = {descreaseByOne} text = "decrease by one "> - 1</button>
+    <button onClick = {setToZero} text = "set to Zero"> 0 </button>
     </div>
   )
 }
